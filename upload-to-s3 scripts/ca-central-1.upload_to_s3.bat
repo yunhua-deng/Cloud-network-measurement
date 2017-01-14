50615c55251cc2fbf3297d03cb3bf1eb6daf8e04
yunhua-deng <yh.deng@hotmail.com>
2017-01-14 16:16:22 +0800

    add some aws s3 related scripts

---
+++ upload-to-s3 scripts/ca-central-1.upload_to_s3.bat
@@ -0,0 +1,2 @@
+start aws s3 cp ping_to_dc.txt s3://latency-measurement//ca-central-1.ping_to_dc.txt 
+start aws s3 cp ping_to_prefix.txt s3://latency-measurement//ca-central-1.ping_to_prefix.txt 
---
+++ upload-to-s3 scripts/eu-west-2.upload_to_s3.bat
@@ -0,0 +1,2 @@
+start aws s3 cp ping_to_dc.txt s3://latency-measurement//eu-west-2.ping_to_dc.txt 
+start aws s3 cp ping_to_prefix.txt s3://latency-measurement//eu-west-2.ping_to_prefix.txt 
