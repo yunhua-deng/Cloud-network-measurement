b4539132c03e9cf147b9b3a741fa9acd51ea7332
yunhua-deng <yh.deng@hotmail.com>
2017-01-20 15:40:12 +0800

    added some batch scripts
    zipping and uploading ping data

---
+++ pinginfoview programs/rename_file_weekly.bat
@@ -0,0 +1,11 @@
+for /l %%g in (1,1,6) do timeout /nobreak 86400
+timeout /nobreak 43200
+rename ping_to_prefix.txt ping_to_prefix.week_2.txt
+
+for /l %%g in (1,1,6) do timeout /nobreak 86400
+timeout /nobreak 43200
+rename ping_to_prefix.txt ping_to_prefix.week_3.txt
+
+for /l %%g in (1,1,6) do timeout /nobreak 86400
+timeout /nobreak 43200
+rename ping_to_prefix.txt ping_to_prefix.week_4.txt
